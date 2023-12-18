12ac5c291f16b8f940ed0c66561fe1b51071d209
Oscar Marin Egea <heartlessome>
2023-12-18 02:07:26 +0100

    Loading Screen upload

---
+++ LoadingScreen/GameSceneManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using TMPro;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameSceneManager : Singleton<GameSceneManager>
+{
+    [SerializeField] private GameObject m_loadingCanvas;
+    [SerializeField] private GameObject[] m_deactivateObjects;
+    [SerializeField] private BarController m_progresBar;
+    [SerializeField] private TMP_Text m_text;
+
+    public async void LoadScene(string scene) {
+        AsyncOperation op = SceneManager.LoadSceneAsync(scene);
+        op.allowSceneActivation = false;
+
+        m_loadingCanvas.SetActive(true);
+        foreach (GameObject go in m_deactivateObjects) go.SetActive(false);
+
+        m_progresBar?.setValue(0);
+
+        if (m_text) m_text.text = scene;
+        AudioManager.Instance.StopMusic();
+
+        // la carga unity la lleva hasta el 90%
+        do {
+            m_progresBar?.setValue(op.progress); 
+
+            // los delays se ponen para poder ver las barras en movimiento
+            // await Task.Yield();
+            await Task.Delay(200);
+        } while (op.progress < 0.9f);
+
+        m_progresBar?.setValue(op.progress); 
+
+        // de nuevo es solo para hacer la prueba
+        await Task.Delay(1000);
+
+        op.allowSceneActivation = true;
+        m_loadingCanvas.SetActive(false);
+    }
+}
+
+#region Editor
+#if UNITY_EDITOR
+[CustomEditor(typeof(GameSceneManager))]
+class GameSceneManagerEditor : Editor {
+
+    SerializedProperty m_loadingCanvas, m_deactivateObjects, m_progresBar, m_text;
+
+    private void OnEnable() {
+        m_loadingCanvas = serializedObject.FindProperty("m_loadingCanvas");
+        m_deactivateObjects = serializedObject.FindProperty("m_deactivateObjects");
+        m_progresBar = serializedObject.FindProperty("m_progresBar");
+        m_text = serializedObject.FindProperty("m_text");
+    }
+
+    public override void OnInspectorGUI() {
+        GameSceneManager script = (GameSceneManager)target;
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(m_loadingCanvas, new GUIContent("Loading Screen Canvas"), true);
+        if (m_loadingCanvas.objectReferenceValue == null) {
+            EditorGUILayout.HelpBox("There is no Canvas attached", MessageType.Warning);
+        }
+
+        EditorGUI.indentLevel++;
+
+        EditorGUILayout.PropertyField(m_deactivateObjects, new GUIContent("Objects to deactivate"), true);
+        EditorGUILayout.PropertyField(m_progresBar, true);
+        EditorGUILayout.PropertyField(m_text, true);
+
+        EditorGUI.indentLevel--;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
+#endregion
